1bf80bd2af3f522897c5625a1a1368e68729cdac
Dana Nobile <danagnobile@gmail.com>
2020-06-17 08:58:18 -0400

    Set up initial files, ran npm for json files

---
+++ letter.js
@@ -0,0 +1,7 @@
+function Letter(character, guessed) {
+    this.character = "";
+    this.makeNoise = function () {
+        if (this.character === true) {
+            console.log(this.noise);
+        }
+    };
