53880cc519b5abe97a6be99fbc66fc648f1ba11b
Dana Nobile <danagnobile@gmail.com>
2020-06-17 10:49:26 -0400

    Established letter.js function for character, and added gitignore file

--- letter.js
+++ letter.js
@@ -1,7 +1,33 @@
-function Letter(character, guessed) {
-    this.character = "";
-    this.makeNoise = function () {
-        if (this.character === true) {
-            console.log(this.noise);
+// Creates a function and defines constructor
+
+function Letter(character) {
+    this.character = character,
+        this.guessed = false,
+        // Method to show either the character or an underscore
+        this.show = function () {
+            // If letter is guessed, the return will be the character 
+            if (this.guessed === false) {
+                return "_";
+            }
+            // If the letter is not guessed, the return will be an underscore
+            else {
+                return this.letter;
+            }
+        };
+    // Method to check if user input is same as character
+    this.checkLetter = function (input) {
+        if (input === this.character) {
+            this.guessed = true;
+            return true;
+        }
+        else {
+            return false;
         }
     };
+
+}
+
+
+// Export the Letter constructor so ability to access in word.js
+
+module.exports = Letter;
